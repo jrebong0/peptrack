154c7234da9d8c88ba5b34aa27c3b746779cead4
avillacrosis <alvan.villacrosis@rcggs.com>
2018-11-14 14:00:49 +0800

    login 1st commit UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
