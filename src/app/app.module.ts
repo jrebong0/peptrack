069f0904436eaf9580f2b5cf3db1615f4b1b36d0
ATDIO <ATDIO@S11134>
2018-11-14 21:08:15 +0800

    [Employee] - Created form for adding employees. No validation as of the moment except for email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { LoginComponent } from './login/login.component';
+import { EmployeeCreateComponent } from './employees/employee-create/employee-create.component';
 
 
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent },
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'employees/create', component: EmployeeCreateComponent }
 ];
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
-    LoginComponent
+    LoginComponent,
+    EmployeeCreateComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
