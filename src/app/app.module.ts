ef83b27e9888b92aeabb7ebbc489bb8684fa2f34
Jonathan Sumiguin <jonathan.sumiguin@rcggs.com>
2018-11-15 23:06:29 +0800

    Fixed items.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent }
+
 ];
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
