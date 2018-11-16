8714759b3c797d99363068e328a38a48a4bc7048
Santos <DSSantos@rcgitmanila.com>
2018-11-16 14:39:03 +0800

    [PEPTRACK] Login Module
    *Basic login feature completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { environment } from '../environments/environment';
 import { TowersComponent } from './towers/towers.component';
+import { EmployeeService } from './services/employee.service';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -44,7 +45,7 @@ const appRoutes: Routes = [
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ EmployeeService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
