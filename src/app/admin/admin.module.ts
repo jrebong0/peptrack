a29a5a7a7eb9229d68ef43c910e90a2a8b663666
Santos <DSSantos@rcgitmanila.com>
2018-11-24 02:25:02 +0800

    [PEPTRACK] New Engagement Screen + Projects Screen

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { AdminRoutingModule } from './admin-routing.module';
-import { TowersComponent } from './towers/towers.component';
 import { StudioComponent } from './studio/studio.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeCreateComponent } from './employees/employee-create/employee-create.component';
@@ -16,11 +15,13 @@ import { AgGridModule } from 'ag-grid-angular';
 import { PermissionsComponent } from './permissions/permissions.component';
 import { AddTowerDialogComponent } from './towers/add-tower-dialog/add-tower-dialog.component';
 import { UpdateTowerDialogComponent } from './towers/update-tower-dialog/update-tower-dialog.component';
-
+import { ProjectsComponent } from './projects/projects.component';
+import { EngagementComponent } from './engagement/engagement.component';
 @NgModule({
   declarations: [
     AdminComponent,
-    TowersComponent,
+    ProjectsComponent,
+    EngagementComponent,
     TowerFilterPipe,
     StudioComponent,
     EmployeesComponent,
