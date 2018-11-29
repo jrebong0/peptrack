232e0d7324b9ea35024d474902372b855b86d30f
avillacrosis <alvan.villacrosis@rcggs.com>
2018-11-29 19:25:42 +0800

    Updated Home Page to Display Engagement Cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import { Router } from '@angular/router';
+import { Engagement } from 'src/app/models/engagement.model';
+import { EngagementService } from 'src/app/services/engagement.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+@NgModule({
+  imports: [
+    CommonModule
+  ]
+})
 export class HomeComponent implements OnInit {
-
+  engagements: Engagement[];
   constructor(
-    private router: Router) { }
+    private router: Router,
+    private engagementService: EngagementService) { }
 
-  ngOnInit() {
+    ngOnInit() {
+      this.getEngagements();
+    }
+  
+    getEngagements() {
+      this.engagementService.getEngagementList().subscribe(
+        (engagementList: any) => {
+          this.engagements = engagementList;
+        }
+      );
+    }
+
+  routeToPerformance(key) {
+    //TODO: Update route including key
+    this.router.navigate(["/performance"]);
   }
 
-  routeToPerformance() {
-    this.router.navigate(["/performance"]);
+  routeToYTDPerformance(key) {
+    //TODO: Update route including key
+    console.log("YTD Clicked! " + key);
   }
 
 }
