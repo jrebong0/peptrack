fa6ce083c75ba2999282e3d6a9f47f73b2ca81b6
avillacrosis <alvan.villacrosis@rcggs.com>
2018-11-29 19:30:16 +0800

    Clean Code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Engagement } from 'src/app/models/engagement.model';
 import { EngagementService } from 'src/app/services/engagement.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
-})
-@NgModule({
-  imports: [
-    CommonModule
-  ]
 })
 export class HomeComponent implements OnInit {
   engagements: Engagement[];
